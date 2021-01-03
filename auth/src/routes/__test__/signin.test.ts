db3c8fdba550565da7526567719fd86ab8b10a0b
Sailesh Dahal <saileshbro@gmail.com>
2021-01-03 21:50:34 +0545

    added tests for signin route

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,42 @@
+import request from 'supertest'
+import { resolveProjectReferencePath } from 'typescript'
+import app from '../../app'
+it('fails when a email that does not exists is supplied', async () => {
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400)
+})
+it('fails when incorrect password is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'fdhjshdf',
+    })
+    .expect(400)
+})
+it('responds with cookie when correct password is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+  const response = await request(app).post('/api/users/signin').send({
+    email: 'test@test.com',
+    password: 'password',
+  })
+  expect(response.get('Set-Cookie')).toBeDefined()
+})
