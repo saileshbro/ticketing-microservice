c05eeae50aefb81a47cb13e77935e4a0cf3ea81b
Sailesh Dahal <saileshbro@gmail.com>
2021-01-06 10:56:37 +0545

    init tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+import { NotFoundError, errorHandler } from '@saileshbrotickets/common'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+)
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+app.use(errorHandler)
+export default app
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,42 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import request from 'supertest'
+import mongoose from 'mongoose'
+import app from '../app'
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>
+    }
+  }
+}
+let mongo: MongoMemoryServer
+beforeAll(async () => {
+  process.env.JWT_KEY = 'DSFKLDSAFKLDSAJFDKAS'
+  mongo = new MongoMemoryServer()
+  const mongoUri = await mongo.getUri()
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+})
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections()
+  for (const coll of collections) {
+    await coll.deleteMany({})
+  }
+})
+afterAll(async () => {
+  await mongo.stop()
+  await mongoose.connection.close()
+})
+global.signin = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201)
+  return response.get('Set-Cookie')
+}
