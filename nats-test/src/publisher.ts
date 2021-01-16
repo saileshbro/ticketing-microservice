ec6ac3a5d8da6ffb42ac3fa5037c93f12798d861
Sailesh Dahal <saileshbro@gmail.com>
2021-01-16 13:02:33 +0545

    changed callback based to promise

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -4,12 +4,16 @@ console.clear()
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 })
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS')
   const publisher = new TicketCreatedPublsher(stan)
-  publisher.publish({
-    id: '7842',
-    title: 'concert',
-    price: 29,
-  })
+  try {
+    await publisher.publish({
+      id: '7842',
+      title: 'concert',
+      price: 29,
+    })
+  } catch (error) {
+    console.error(error)
+  }
 })
